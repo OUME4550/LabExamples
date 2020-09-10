46a040e7bcbbc34d220599a93759441406ef460f
jaywilhelm <jwilhelm@ohio.edu>
2020-09-10 10:56:55 -0400

    added analog read with float example

---
+++ analogRead_example.ino
@@ -0,0 +1,37 @@
+#define A2DPin A0
+#define R1_fixed 10000.0
+//define our function prototype
+float readTempC(const int TPin, const float R1);
+void setup() 
+{
+  //setup the serial port for debug info...
+  Serial.begin(115200);
+  Serial.println("Analog read...");
+}
+void loop() 
+{
+  //read the digital value 0-1023
+  int digValue = analogRead(A2DPin);
+  
+  float failure = digValue / 1024;//THIS WILL NOT WORK!!!!
+  //The compiler only sees int values to the right of =
+  //So 0/1024 in int land = 0 always
+  
+  //instead, make the constant value a FLOAT
+  //by adding decimal place after
+  float maybe = digValue / 1024.0;
+  
+  //better yet, typecast it and make the int a float
+  float works = (float)digValue;
+  works = works / 1024.0;
+    
+  Serial.print("fails: ");
+  Serial.println(failure);
+  
+  Serial.print("maybe: ");
+  Serial.println(maybe);
+  
+  Serial.print("works: ");
+  Serial.println(works);
+  delay(1000);
+}
